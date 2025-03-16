01f4634bf2a5c2d5e92642889298a62f162599f1
MasterAtla <masteratlausernoob@gmail.com>
2025-03-15 19:03:35 -0600

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const boton = document.getElementById("mostrarVideo");
-const videoContainer = document.getElementById("videoContainer");
-const video = document.getElementById("mivideo");
 
-boton.addEventListener("click", () => {
-    // Alternamos la visibilidad del video
-    if (videoContainer.classList.contains("oculto")) {
-        videoContainer.classList.remove("oculto");
-        video.play();  // Inicia el video cuando es visible
-    } else {
-        videoContainer.classList.add("oculto");
-        video.pause();  // Pausa el video cuando se oculta
-    }
-});
